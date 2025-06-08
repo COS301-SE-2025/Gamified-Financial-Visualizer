183d26deff25ccb501416e29be040edeb07c04ac
MissNcube <aundreancu@gmail.com>
2025-06-08 18:36:13 +0200

    index.ts for learning modules blueprint

--- api-backend/modules/learning/index.ts
+++ api-backend/modules/learning/index.ts
@@ -2,3 +2,13 @@
  * @file index.ts
  * @desc for Exportng routes for learning modules and lessons.
  */
+
+import { Router } from 'express';
+import learningRoutes from './routes/learningRoutes';
+
+const router = Router();
+
+router.use('/', learningRoutes);
+
+export default router;
+
