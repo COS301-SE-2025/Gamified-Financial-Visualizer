9801da8cd867f45ba5d4ab07194867a37e41d362
MissNcube <aundreancu@gmail.com>
2025-07-23 12:54:58 +0200

    experimenting redis

--- api-backend/config/redis.ts
+++ api-backend/config/redis.ts
@@ -4,17 +4,27 @@ import { RedisOptions } from 'bullmq';
 import { createClient } from 'redis';
 import Redis from 'ioredis';
 
-export const redisConnection: RedisOptions = {
-  host: process.env.REDISHOST,
-  port: parseInt(process.env.REDIS_PORT || '6379', 10),
-};
+// export const redisConnection: RedisOptions = {
+//   host: process.env.REDISHOST,
+//   port: parseInt(process.env.REDIS_PORT || '6379', 10),
+// };
+
+// export const redisClient = createClient({
+// socket: {
+//     host: redisConnection.host,
+//     port: redisConnection.port,
+//   },
+//   password: process.env.REDIS_PASSWORD,});
+
+// redisClient.on('error', err => console.error('[Redis] Client Error', err));
+
+// (async () => {
+//   await redisClient.connect();
+// })();
 
 export const redisClient = createClient({
-socket: {
-    host: redisConnection.host,
-    port: redisConnection.port,
-  },
-  password: process.env.REDIS_PASSWORD,});
+  url: process.env.REDIS_URL,
+});
 
 redisClient.on('error', err => console.error('[Redis] Client Error', err));
 
