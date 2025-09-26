ccb5dc1fb3b2e83618e72f11e5179b9c2e1f1b87
MissNcube <aundreancu@gmail.com>
2025-09-26 18:57:14 +0200

    Just finished editing AI files to point to deployed AI service instead of localhost.

--- api-backend/modules/ai/routes/insightsRouter.ts
+++ api-backend/modules/ai/routes/insightsRouter.ts
@@ -6,6 +6,9 @@ import { redisClient } from '../../../config/redis';
 import * as insightsService from '../services/insights.service';
 const router = express.Router();
 
+const AI_URL = process.env.REACT_APP_AI_URL || 'http://gamified-finance-ai-avf0gsfrf5a4b9cj.southafricanorth-01.azurewebsites.net/';
+// const AI_URL = 'http://localhost:6000'; 
+
 // Endpoint to fetch user insights on income and expenses per month in the current year
 router.get('/transactions/:userId', async (req, res) => {
    const { userId } = req.params;
@@ -495,7 +498,7 @@ router.get('/radar/:userId', async (req, res) => {
    }
 });
 
-const INSIGHTS_BASE = "http://localhost:6000/insights";
+const INSIGHTS_BASE = `${AI_URL}/insights`;
 
 // 1) Month + User → Wrapped insights
 //    e.g. GET /sentiment/user/42/7  → proxies to GET http://localhost:6000/insights/42/7
@@ -534,7 +537,7 @@ router.get("/sentiment/user/:userId/:month", async (req, res) => {
 
       // send JSON to Python endpoint
       const { data } = await axios.post(
-         `http://localhost:6000/insights/user/${userId}/${month}`,
+         `${AI_URL}/insights/user/${userId}/${month}`,
          userData
       );
 
@@ -663,7 +666,7 @@ router.get('/trends/:userId', async (req, res) => {
       };
 
       // Fetch trends data from the database
-      const trends = await axios.post(`http://localhost:6000/insights/trends/`,
+      const trends = await axios.post(`${AI_URL}/insights/trends/`,
          userData
       );
 
